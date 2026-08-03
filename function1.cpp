eeebeca482bb5fce1c50fc4a1f438380ee639385
AdityaOP007 <adityaprakashjha321@gmai.com>
2026-08-03 23:32:12 +0530

    functions

---
+++ function1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//function defination
+void printHello() {
+    cout << "Hello, World!" << endl;
+}
+int main() {
+    //function call
+    printHello();
+    return 0;
+}
