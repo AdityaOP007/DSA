2ba52896d4b6f30731a547ed35264f6eb29fa1b6
AdityaOP007 <adityaprakashjha321@gmai.com>
2026-08-04 02:12:17 +0530

    arrays

---
+++ arrays3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int nums[] = {10, 20, 30, 1, -15, -24};
+    int size = 6;
+
+    int smallest = INT_MAX;
+
+    for (int i = 0; i < size; i++) {
+        if (nums[i] < smallest) {
+            smallest = nums[i];
+        }
+    }
+
+    cout << smallest << endl;
+
+    return 0;
+}
