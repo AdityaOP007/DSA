2681bfd9ec6ef5d98f6efa6f2d7bf77546bfba07
Aditya prakash jha <adityaprakashjha321@gmail.com>
2026-08-07 15:41:23 +0530

    Refactor binary search implementation for clarity

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
 int binarySearch(int arr[], int n, int target) {
     int st = 0;
     int end = n - 1;
 
     while (st <= end) {
-        int mid = (st + end) / 2;
+        int mid = st + (end - st) / 2;
 
-        if (arr[mid] == target) {
-            return mid; // Target found at index mid
-        } else if (arr[mid] < target) {
-            st = mid + 1; // Search in the right half
-        } else {
-            end = mid - 1; // Search in the left half
-        }
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            st = mid + 1;
+        else
+            end = mid - 1;
     }
 
-    return -1; // Target not found
+    return -1;
 }
+
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]); // Get the size of the array
+    int n = sizeof(arr) / sizeof(arr[0]);
     int target = 3;
+
     int result = binarySearch(arr, n, target);
 
-    if (result != -1) {
+    if (result != -1)
         cout << "Element found at index " << result << endl;
-    } else {
+    else
         cout << "Element not found" << endl;
-    }
 
     return 0;
 }
